bf5fa646499cf590aab2fc6055ed302e7f3bf10d
KJBBato <bato.kevin@gmail.com>
2020-03-10 21:40:51 -0400

    insert: commented on test cases and modified change on axis

--- lib/matplotlib/axis.py
+++ lib/matplotlib/axis.py
@@ -1672,7 +1672,7 @@ class Axis(martist.Artist):
         """
         # XXX if the user changes units, the information will be lost here
         ticks = self.convert_units(ticks)
-        if len(ticks) > 0:
+        if len(ticks) > 0: # now handles ticks with length greater than 1
             xleft, xright = self.get_view_interval()
             if xright > xleft:
                 self.set_view_interval(min(ticks), max(ticks))
--- lib/matplotlib/tests/test_xticks.py
+++ lib/matplotlib/tests/test_xticks.py
@@ -3,6 +3,7 @@ from matplotlib.testing.decorators import image_comparison
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Test for setting one tick on the x-axis  
 @image_comparison(baseline_images=['xticks'], extensions=['png'])
 def test_xticks():
     _, ax3 = plt.subplots(1, 1, figsize=(3, 3))
@@ -10,6 +11,7 @@ def test_xticks():
         ax.plot([1, 2])
     ax3.set_xticks([2])
 
+# Test for setting one tick on the x-axis for a large scale
 @image_comparison(baseline_images=['xticks_large_scale'], extensions=['png'])
 def test_xticks_large_scale():
     _, ax3 = plt.subplots(1, 1, figsize=(3, 3))
@@ -17,6 +19,7 @@ def test_xticks_large_scale():
         ax.plot([1, 2])
     ax3.set_xticks([100])
 
+# Test for setting one tick on the x-axis for a smaller scale
 @image_comparison(baseline_images=['xticks_small_scale'], extensions=['png'])
 def test_xticks_small_scale():
     _, ax3 = plt.subplots(1, 1, figsize=(3, 3))
@@ -24,7 +27,7 @@ def test_xticks_small_scale():
         ax.plot([1, 2])
     ax3.set_xticks([0.5])
     
-
+# Test for normal behavior with no ticks set 
 @image_comparison(baseline_images=['xticks_empty'], extensions=['png'])
 def test_xticks_empty():
     _, ax3 = plt.subplots(1, 1, figsize=(3, 3))
@@ -32,6 +35,7 @@ def test_xticks_empty():
         ax.plot([1, 2])
     ax3.set_xticks([])
 
+# Test for normal behavior with a tick range 
 @image_comparison(baseline_images=['xticks_range'], extensions=['png'])
 def test_xticks_range():
     _, ax3 = plt.subplots(1, 1, figsize=(3, 3))
